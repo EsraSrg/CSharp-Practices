bf1aa8b9f6bc173c20c9eece760636bbb258e831
Esra Sergiser <128863910+EsraSrg@users.noreply.github.com>
2024-09-12 21:40:39 +0300

    koşul ifadeleri

---
+++ wissen/KararYapilari/Program.cs
@@ -0,0 +1,94 @@
+﻿// See https://aka.ms/new-console-template for more information
+// 2 farklı koşul yöntemi var:
+// 1) ıf else
+// 2) switch case yöntemi
+
+#region If yöntemi
+// Koşul değişkeni her zaman mantıksal operatörlerden gelen bir değer olmalı(true,false)
+/*
+
+bool isOk=true;
+
+Console.WriteLine("A");
+if (isOk)
+{
+  Console.WriteLine("B");
+}
+Console.WriteLine("C")
+
+// if içine değişken yerine mantıksal deger üretecek bir kod bloğu yazılabilir.
+
+Console.WriteLine("değer girin..");
+int intDegisken=int.Parse(Console.ReadLine());
+if(intDegisken>5)
+{
+  Console.WriteLine("Girilen deger 5'den büyük.");
+}
+
+*/
+#endregion
+
+/*
+Console.WriteLine("harf girin, küçük ya da büyük");
+char karakter=char.Parse(Console.ReadLine());
+int intDegisken=Convert.ToInt32(karakter);
+
+if(intDegisken>64 && intDegisken<91)
+{
+  Console.WriteLine("büyük harf")
+}
+else{
+ Console.WriteLine("küçük harf");
+}
+     
+
+System.Console.WriteLine("Bir karakter girin:");
+
+char charDegisken = char.Parse(Console.ReadLine());
+
+if (charDegisken >= 'A' && charDegisken <= 'Z') 
+{
+    Console.WriteLine("Büyük Harf");
+}
+
+else{
+    Console.WriteLine("Küçük Harf");
+}
+
+
+
+
+System.Console.WriteLine( "Kullanıcı adınızı girin..");
+string userName=Console.ReadLine();
+System.Console.WriteLine("Şifrenizi girin");
+string password=Console.ReadLine();
+
+if(userName=="wissen" && password=="1010")
+{
+    System.Console.WriteLine("Giriş Başarılı..");
+}
+else{
+    System.Console.WriteLine(   "Giriş Başarısız");
+}
+
+
+
+using System.Runtime.InteropServices;
+
+System.Console.WriteLine("Fiyatı giriniz..");
+double fiyat=double.Parse(Console.ReadLine());
+
+if(fiyat>100)
+{ fiyat=fiyat+fiyat*0.2;
+    Console.WriteLine(fiyat);
+}
+else{
+    fiyat=fiyat-fiyat*0.3;
+    System.Console.WriteLine(fiyat);
+}
+*/
+
+#region İÇ İÇE İF
+
+
+#endregion
