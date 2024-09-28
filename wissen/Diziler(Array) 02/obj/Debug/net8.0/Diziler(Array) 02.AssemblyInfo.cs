7b73938fddb79ac10469007eba0dceb1e9a38ee6
Esra Sergiser <128863910+EsraSrg@users.noreply.github.com>
2024-09-28 23:17:43 +0300

    array , queue

--- wissen/Diziler(Array) 02/obj/Debug/net8.0/Diziler(Array) 02.AssemblyInfo.cs
+++ wissen/Diziler(Array) 02/obj/Debug/net8.0/Diziler(Array) 02.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Diziler(Array)")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Diziler(Array) 02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9be7453a43d5dc611cafc5f3149cc3d69a825955")]
-[assembly: System.Reflection.AssemblyProductAttribute("Diziler(Array)")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Diziler(Array)")]
+[assembly: System.Reflection.AssemblyProductAttribute("Diziler(Array) 02")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Diziler(Array) 02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
--- wissen/Diziler(Array)/Program.cs
+++ wissen/Diziler(Array)/Program.cs
@@ -237,3 +237,209 @@ for(int i=0; i<reverseArray.Length;i++){
     Console.WriteLine(destinationArray[i]);
 
 }
+
+//---------------------------------------------------------------------------------------------
+// Array.Sort diziyi sıralar(rakamları küçükten büyüğe, harfleri ise, alfabetik sıraya göre sıralar)
+/*Array.Sort(reverseArray);
+for(int i=0;i<reverseArray.Length;i++){
+    Console.WriteLine(reverseArray[i]);
+}*/
+
+
+// Array.Clear diziyi temizlemek için kullanılır(Dizi içerisindeki tüm elemanlar siliniz);
+//Array.Clear(reverseArray);
+
+// Array.Copy
+// bir dizinin kopyasını oluşturmak için Array.Copy fonksiyonu kullanılabilir
+
+
+/*int[] destinationArray = new int[5];
+Array.Copy(reverseArray,destinationArray,reverseArray.Length);
+for(int i=0; i<reverseArray.Length;i++){
+    Console.WriteLine(destinationArray[i]);
+
+}
+*/
+
+// Örnek
+// Kullanıcıdan 10 adet sayı alınız.
+// bu sayıların tek sayı olanları tek sayi dizisine, çift sayı olanları çift sayı dizine atalım
+
+/*int[] ciftDizi = new int[10];
+int[] tekDizi = new int[10];
+
+for(int i = 0; i <ciftDizi.Length;i++){
+
+    Console.WriteLine("Bir sayı giriniz");
+    int sayi = int.Parse(Console.ReadLine());
+    if(sayi%2==0){
+
+        ciftDizi[i]=sayi;
+    }
+    else{
+        tekDizi[i]=sayi;
+    }
+}
+// Dizileri listeyelim 
+for(int i = 0;i<ciftDizi.Length;i++){
+
+    Console.WriteLine("Çift Dizi {0}",ciftDizi[i]);
+    Console.WriteLine("Tek Dizi  {0}",tekDizi[i]);
+}
+*/
+
+
+// Örnek : 
+// Bir string dizisi içerisinde, 10 adet isim giriniz
+
+// daha sonrada  bu 10 adet isim dizisinin, içerisinde gezip, ilk harf ve son harfi gösterip, 
+// aradaki harfleri ** yapıp yeni diziye atan uygulama
+
+// Örnek : Elif -> E**f
+// Hüsamettin -> H********n
+/*string[] isimler = new string[10];
+string[] gizliIsimler = new string[10];
+isimler[0]="Ayşe";
+isimler[1]="Ali";
+isimler[2]="Selçuk";
+isimler[3]="Pınar";
+isimler[4]="Orhun";
+isimler[5]="Ebru";
+isimler[6]="Barış";
+isimler[7]="Özlem";
+isimler[8]="Alihan";
+isimler[9]="Bahadır";
+
+for(int i=0;i<isimler.Length;i++){
+    // isimler dizisi içerisinde dön!!
+    
+    // ismin ilk karakteri için
+    string start = isimler[i].Substring(0,1);
+    // ismin son karakteri için
+    string end = isimler[i].Substring(isimler[i].Length-1,1);
+    //string newIsim ="";
+
+    // V1
+    /*for(int j=0;j<isimler[i].Length-2;j++){
+
+         newIsim +="*";
+         
+    }
+
+    //V2
+    //gizliIsimler[i]=start.PadRight(isimler[i].Length-1,'*')+end;
+
+}
+// işler bitti, ekrana yazdıralım
+for(int i=0;i<gizliIsimler.Length;i++){
+    Console.WriteLine(gizliIsimler[i]);
+}
+*/
+
+// Örnek 
+// Uygulama 5 adet not istesin, ve bu notlari dizide tutalim, 
+//notlari aldiktan sonra kullaniciya asagidaki soruyu sorup, geregini yapalim. 
+//Degistirmek istediginiz not varsa 1 e basin, (hangi notun degisecegini sorup degeri 
+//sormak gerekecek) notlari siralanmis listelemek icin 2 e basin, cikmak icin 3 e basin.
+/*int[] nots = new int[5];
+
+
+ for(int i=0; i<5;i++){
+    Console.WriteLine("{0}. notu giriniz",i+1);
+    nots[i]=int.Parse(Console.ReadLine());
+
+ }
+
+// uygulama exit yapana kadar sonsuza kadar dönecek
+ while(true){
+ //Not alımı bitti
+ Console.WriteLine("Not alımı bitti");
+ Console.WriteLine("Aşağıdaki işlemleri yapabilirsiniz");
+ Console.WriteLine("Not Değiştirmek için 1");
+ Console.WriteLine("Not Sıralamak için 2");
+ Console.WriteLine("Çıkmak için 3'e basınız");
+ string secim = Console.ReadLine();
+ if(secim=="1"){
+
+    //[1][3][4][5][100][2]
+    //100 değerini 500 değeri ile değiştir
+    Console.WriteLine("Not değiştirme bölümüme hoşgeldiniz");
+    Console.WriteLine("Değiştirmek istediğiniz notu giriniz");
+    int not = int.Parse(Console.ReadLine());
+    Console.WriteLine("Yeni notu giriniz");
+    int newNot = int.Parse(Console.ReadLine());
+    int arrayIndexNo = Array.IndexOf(nots,not);
+    nots[arrayIndexNo]=newNot;
+    for(int i=0; i<nots.Length;i++){
+        Console.WriteLine(nots[i]);
+    }
+ }
+ else if(secim=="2"){
+
+    Array.Sort(nots);
+    for(int i=0; i<nots.Length;i++){
+
+        Console.WriteLine(nots[i]);
+    }
+ }
+ else if(secim=="3"){
+
+    // uygulama sonlandı
+    break;
+ }
+ }
+ */
+
+ // Örnek Anlatım
+
+string[] isimler = new string[3];
+isimler[0]="Ayşe";
+isimler[1]="Ali";
+isimler[2]="Mehmet";
+
+// Örnek : Örnekteki dizinin ilk harflerini ekrana yazdırınız
+for(int i=0;i<3;i++){
+
+    Console.WriteLine(isimler[i][0]);
+
+}
+// yukarıdaki örnekte, isimler dizisi içerisindeki string değer ekrana yazdırılıyor.
+// string bir idade de ayrıca bir dizi olduğu için tekrar index numarası ile char değer çağrılabilir.
+// String bir ifade, char dizisidir.
+string deger = "wissen";
+Console.WriteLine(deger[0]);
+// bu sebeple, string ifade de istenilen karaktere ulaşmak için [] kullanılabilir.
+
+// Unutmamanız için tekrar
+// DİKKAT !!!! String bir char dizisidir.
+
+// string'in char dizisi olması ile ilgili bir örnek yapalım
+// 10 adet ismin olduğu bir dizi alınız kullanıcıdan,
+// dizi giriş tamamlandıktan sonra, 
+// dizi içerisinde isimlerin, tersten yazılışlarını ekrana yazdırınız
+
+string[] names = new string[10];
+for(int i =0;i<10;i++){
+
+    Console.WriteLine("{0}. ismi giriniz",i+1);
+    names[i]=Console.ReadLine();
+}
+for(int i =0;i<names.Length;i++){
+// dizi içerisinde dönmek için
+    string ters ="";
+   for (int j = names[i].Length - 1; j>= 0 ; j--)
+   {
+        ters+=names[i][j];
+        // i = 0  = Ali - > j=2 = i
+        // i = 0  = Ali - > j=1 = l
+   }
+   Console.WriteLine(ters);
+}
+
+//Ali    i = 0 
+//Veli   i = 1
+//Mehmet i = 2
+
+// A  j = 0
+// l  j = 1
+// i =j = 2
