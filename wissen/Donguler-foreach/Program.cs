539a098aa37d8d80f5dd3131adf14c3d769d0332
Esra Sergiser <128863910+EsraSrg@users.noreply.github.com>
2024-10-01 21:43:16 +0300

    foreach

---
+++ wissen/Donguler-foreach/Program.cs
@@ -0,0 +1,121 @@
+﻿// foreach dongusu for ve while dongusunden farklı olrak bir döngü değişkenine ihtiyaç duymaz.
+//artırım ifadesine de ihtiyaç duymaz.
+// bir koşula bağlı da dönmez.
+
+//tanım
+
+// arrayList icinde donelim.
+
+using System.Collections;
+
+/*
+ArrayList arrayList=new ArrayList();
+
+arrayList.Add("Istanbul");
+arrayList.Add("Izmir");
+arrayList.Add("Rize");
+arrayList.Add("muğla");
+arrayList.Add("maras");
+arrayList.Add("hatay");
+
+foreach(object item in arrayList){
+
+    System.Console.WriteLine(item);
+}
+
+//object: koleksiyon icindeki eleman tipi ne ise o tip olmalı.
+// item: dongu her tur dondugunde, dongu icerisindeki o anki elemandır.
+//in :  bir keyworddur.
+// arraylist: bu kısma döneceginiz koleksiyonu yazmalısınız.
+
+// başı ve sonu belli olan koleksiyonlarda koleksiyon sonuna kadar döner..şarta ihtiyaç duymaz.
+
+// foreach her zaman belli bir istisna hariç : her zaman ileri yonlu iterasyon yapar.
+
+// break continue gibi atlama deyimleri bu dongude de vardir.
+
+
+// key value koleksiyonu için döndüreceksen DictionaryEntry ile yapabilrsin
+
+/*
+SortedList sortedList=new SortedList();
+
+sortedList.Add("1","adana");
+sortedList.Add("2","rize");
+sortedList.Add("3","izmir");
+
+foreach(DictionaryEntry item in sortedList){
+    System.Console.WriteLine("key:{0} value:{1}",item.Key,item.Value);
+
+}
+
+//örnek: kullanıcıdan aldıgın not sayisina göre not toplamı ve ortalamayi yazdir.
+
+ArrayList notlar=new ArrayList();
+
+int toplam=0;
+
+System.Console.WriteLine("kaç not gireceksiniz?");
+int notSayisi=int.Parse(Console.ReadLine());
+
+for(int i=0;i<notSayisi;i++){
+    System.Console.WriteLine("{0}. notu giriniz.",i+1);
+    int not=Convert.ToInt32(Console.ReadLine());
+    notlar.Add(not);
+}
+
+
+foreach(int not in notlar){
+    toplam+=not;
+
+}
+System.Console.WriteLine("toplam:{0} ortalama:{1}",toplam,toplam/notSayisi);
+
+*/
+
+// dizi icinde donelim
+
+string [] teknoloji={"c#","docker","kubernetes","rust","css","RabbitMq","go","java"};
+
+
+foreach(string tech in teknoloji){
+    System.Console.WriteLine(tech);
+}
+
+
+int [] plakalar={35,09,25,35,29};
+
+foreach(int plaka in plakalar){
+    System.Console.WriteLine("plaka:{0}",plaka);
+}
+
+Random [] randoms={new Random(), new Random(), new Random()};
+
+foreach(Random rnd in randoms){
+    System.Console.WriteLine(rnd.Next());
+}
+
+
+
+int [] randomlar ={new Random().Next(),new Random().Next(),new Random().Next()};
+
+foreach(int random in randomlar){
+
+    System.Console.WriteLine(random);
+}
+
+//carpim tablosu
+
+
+int [] nums={1,2,3,4,5,6,7,8,9,10};
+
+foreach(int i in nums){
+
+    foreach(int j in nums){
+
+        System.Console.WriteLine("{0}x{1}={2}",i,j,i*j);
+
+
+    }
+    System.Console.WriteLine("---------------");
+}
---
+++ wissen/SortedList-Koleksiyonlar/Program.cs
@@ -0,0 +1,100 @@
+﻿// SortedList : önceki koleksiyonlardan farklı olarak key ve value çifti taşırlar.
+
+//syntax
+
+using System.Collections;
+
+// SortedList sortedList=new SortedList();
+
+// sortedList.Add(1,"Ali");
+// sortedList.Add(3,"Hande");
+// sortedList.Add(9,"Esra");
+// sortedList.Add(7,"selçuk");
+
+//SortedList Koleksiyonu key ve value degerlerini object tipinden aldıgı için degerleri int ya da string girebiliriz.
+// key degerlerine göre sıralama islemi yapar.
+//Ekrana yazdırma:
+
+/*
+for(int i=0;i<sortedList.Count;i++){
+
+    string value=sortedList.GetByIndex(i).ToString(); 
+    Console.WriteLine(value);
+}
+*/
+
+// key ve value tasıdıgi icin Contains(arama) fonk iki tanedir.
+// ConstainsKey ve ContainsValue key arama ve value arama icin kullanılır.
+
+/*
+bool isContainsKey=sortedList.ContainsKey(1);
+
+System.Console.WriteLine(isContainsKey);
+
+bool isContainsValue=sortedList.ContainsValue("Esra");
+System.Console.WriteLine(isContainsValue);
+*/
+
+SortedList sortedList=new SortedList();
+
+sortedList.Add(01, "Adana");
+sortedList.Add(02, "Adıyaman");
+sortedList.Add(03, "Afyonkarahisar");
+sortedList.Add(04, "Ağrı");
+sortedList.Add(05, "Amasya");
+sortedList.Add(06, "Ankara");
+sortedList.Add(07, "Antalya");
+sortedList.Add(08, "Artvin");
+sortedList.Add(09, "Aydın");
+sortedList.Add(10, "Balıkesir");
+sortedList.Add(11, "Bilecik");
+sortedList.Add(12, "Bingöl");
+sortedList.Add(13, "Bitlis");
+sortedList.Add(14, "Bolu");
+sortedList.Add(15, "Burdur");
+sortedList.Add(16, "Bursa");
+sortedList.Add(17, "Çanakkale");
+sortedList.Add(18, "Çankırı");
+sortedList.Add(19, "Çorum");
+sortedList.Add(20, "Denizli");
+sortedList.Add(21, "Diyarbakır");
+sortedList.Add(22, "Edirne");
+sortedList.Add(23, "Elazığ");
+sortedList.Add(24, "Erzincan");
+sortedList.Add(25, "Erzurum");
+sortedList.Add(26, "Eskişehir");
+sortedList.Add(27, "Gaziantep");
+sortedList.Add(28, "Giresun");
+sortedList.Add(29, "Gümüşhane");
+sortedList.Add(30, "Hakkari");
+sortedList.Add(31, "Hatay");
+sortedList.Add(32, "Isparta");
+sortedList.Add(33, "Mersin");
+sortedList.Add(34, "İstanbul");
+sortedList.Add(35, "İzmir");
+sortedList.Add(36, "Kars");
+sortedList.Add(37, "Kastamonu");
+sortedList.Add(38, "Kayseri");
+sortedList.Add(39, "Kırklareli");
+sortedList.Add(40, "Kırşehir");
+sortedList.Add(41, "Kocaeli");
+sortedList.Add(42, "Konya");
+sortedList.Add(43, "Kütahya");
+sortedList.Add(44, "Malatya");
+sortedList.Add(45, "Manisa");
+sortedList.Add(46, "Kahramanmaraş");
+sortedList.Add(47, "Mardin");
+sortedList.Add(48, "Muğla");
+sortedList.Add(49, "Muş");
+sortedList.Add(50, "Nevşehir");
+
+
+for(int i=0;i<sortedList.Count;i++){
+
+    string value=sortedList.GetByIndex(i).ToString(); 
+    
+    Console.WriteLine((i+1)+" "+value);
+}
+
+
+// 4 koleksiyona baktık. Generic ile sonra devam edecegiz.
