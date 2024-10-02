f78fb276b8cf5b846b8fe50000635e90488c1b05
Esra Sergiser <128863910+EsraSrg@users.noreply.github.com>
2024-10-02 19:03:43 +0300

    namespace, queue koleksiyon , arrayList

--- wissen/ArrayList/Program.cs
+++ wissen/ArrayList/Program.cs
@@ -42,7 +42,7 @@
 //     Console.WriteLine("silinen:{0} - Koleksiyon sayısı {1}",silinen,stack.Count);   
 // }
 
-//koleksiyonlar eleman girdikçr grnişleyen yapıdadır.
+//koleksiyonlar eleman girdikçe grnişleyen yapıdadır.
 
 ArrayList arraylist=new ArrayList();  // içerisine object değer alır
    
@@ -55,8 +55,105 @@ int count=arraylist.Count;
 //arrayList'in alabilecegi eleman sayısını belirtir. Capacity siz eleman girdikçe artar.
 int capacity=arraylist.Capacity;
 
-// arrayList icinden veri cekmek icin , dizilerdekş gibi.
+// arrayList icinden veri cekmek icin , dizilerdeki gibi.
 // string deneme=arraylist[0].ToString();
+
+
+
+// Öğrenciden 10 adet not alıp bu notların toplamı ve ortalamsını ekrana yazdıralım
+
+/*ArrayList notlar = new ArrayList();
+for (int i = 0; i < 10; i++){
+
+    Console.WriteLine("{0}. notu giriniz",i+1);
+    notlar.Add(Console.ReadLine());
+}
+Console.WriteLine("Not girişi tamamlandı, entera basarak not bilgilerini alabilirsiniz");
+Console.ReadLine();
+
+int toplamNot = 0;
+for (int i = 0;i<notlar.Count;i++){
+
+  toplamNot+=int.Parse(notlar[i].ToString());
+
+}
+Console.WriteLine("Notlatın toplamı : {0} Ortalamsı : {1}",toplamNot,toplamNot/notlar.Count);
+*/
+
+// Sayısal loto oyununu arraylist ile yapınız
+
+/*ArrayList kullanicidanGelen = new ArrayList();
+ArrayList random=new ArrayList();
+
+ArrayList bilinenSayilar = new ArrayList();
+
+Random rnd = new Random();
+for(int i=0;i<6;i++){
+
+    Console.WriteLine("{0} sayıyı giriniz",i+1);
+    kullanicidanGelen.Add(int.Parse(Console.ReadLine()));   
+    random.Add(rnd.Next(1,49));
+}
+
+Console.WriteLine("Tahminlerin bitti, çekilişi görmek için enter");
+Console.ReadLine();
+
+// iki tane koleksiyonu kartezyen biçimde karşılaştıralım
+for(int i=0;i<kullanicidanGelen.Count;i++){
+
+    for(int j=0;j<random.Count;j++){
+        // burada iki koleksiyonu birbiri ile karşılaştıralım
+       if(Convert.ToInt32(kullanicidanGelen[i])==Convert.ToInt32(random[j])){
+
+            // sayı bilinmiş demektir.
+            bilinenSayilar.Add(kullanicidanGelen[i]); 
+
+       }
+
+    }
+}
+// Bilinen sayıları ekrana yazdıralım
+Console.WriteLine("Bilinen Sayılar");
+for(int i=0;i<bilinenSayilar.Count;i++){
+
+    Console.Write("{0}-",bilinenSayilar[i]);
+}
+*/
+
+// 1 ay sonraya not : 
+
+
+// Koleksiyonların fonksiyonlarına göz atalım
+
+ArrayList liste  = new ArrayList();
+
+// Koleksiyonun içerisindeki verileri siler
+//liste.Clear()
+
+// Koleksiyon içerisindeki verileri sıralar
+liste.Sort();
+
+// Koleksiyon içerisindeki verileri tersten sıralar
+liste.Reverse();
+
+// Koleksiyonu diziye dönüştürür
+object[] objectArray = liste.ToArray();
+
+// koleksiyon içerisinde arama yapar, verdiğiniz değer koleksiyon içerisinde varsa, true yoksa false döndürür.
+liste.Contains("10");
+
+//Insert fonksiyonu, koleksiyon içerisine bir veriyi eklemek için kullanılır
+// iki tane parametre alır.  ilk parametre hangi index'e eklemek istediğiniz. 
+// ikinci paramtre ise verinin kendisidir.
+liste.GetRange(1,10);
+// GetRange fonksiyonu, koleksiyon içerisinden birden fazla veri getirmek için kullanılır
+// verinin geleceği index ve bu indexden sonra kaç index veri getirilsin parametreleri ile çalışır
+liste.Insert(4, "Insert et");
+
+// Koleksiyonlarda count ve capacity birbirine eşitlemek için kullanılır.
+
+// Örnek : Count : 5 Capacity : 8 ise, TrimToSize yapılınca, count ve capacity birbirine eşit oluyor.
+liste.TrimToSize();
 
 
 
---
+++ wissen/Queue-Koleksiyonlar/Program.cs
@@ -0,0 +1,56 @@
+﻿//Queue Dizisi : İçerisinde tutulan değerlerin object olarak tutulduğu bir dizidir.
+// Queue kuyruk mantığında çalışır. (Akbil,Pide,Otobüs,Araba yıkama kuyruğu)
+Queue queue = new Queue();
+
+//Eleman eklemek için 
+queue.Enqueue("Renault");
+queue.Enqueue("Mercedes");
+queue.Enqueue("Audi");
+queue.Enqueue("BMW");
+queue.Enqueue("Anadol");
+queue.Enqueue("togg");
+queue.Enqueue("fiat");
+queue.Enqueue("kia");
+queue.Enqueue("tesla");
+queue.Enqueue("opel");
+queue.Enqueue("cerry");
+queue.Enqueue("hyundai");
+// dizinin içerisine 4 adet eleman yerleştirdik.
+
+// Dizinin boyutunu öğrenelim - Count ile dizinin boyutunu öprenebiliriz.
+Console.WriteLine(queue.Count);
+
+// queue koleksiyonu kuyruk mantığında çalışır.
+// Peek kuyruktan çıkacak ilk elemanı verir. Ancak ilk elemanı silmez.
+//string silinen = queue.Peek().ToString();
+//Console.WriteLine(silinen);
+
+
+ 
+//string que1 = queue.Dequeue().ToString();
+//Console.WriteLine(que1);
+// Queue dizisi, First In First Out mantığı çalışır.
+// Yani ilk giren ilk çıkar mantığında çalışmaktadır.
+
+// bir döngü kullanarak, queue dizinden, elemanları çıkartalım
+
+int sayi = queue.Count;
+for(int i = 0;i<sayi;i++){
+
+    Console.WriteLine("Diziden eleman çıkartmak için enter basınız");
+    Console.ReadLine();
+    string cikan = queue.Dequeue().ToString();
+    Console.WriteLine(cikan);
+    Console.WriteLine("Dizinin elaman sayısı : {0}",queue.Count);
+
+}
+
+// Diziler ve koleksiyonlar.
+// Diziler ile koleksiyonlar arasında tek fark, koleksiyon tanımlarken başlangıç değeri zorunda değildir.
+
+
+
+// Stack Queue'dan farklı olarak, LIFO yani son giren ilk çıkar prensibinde çalışır.
+
+
+
