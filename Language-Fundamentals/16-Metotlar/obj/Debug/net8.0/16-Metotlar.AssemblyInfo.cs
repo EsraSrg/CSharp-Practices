d094cbc6c6e95ee1b45d71b8e51a02c4bce52093
Esra Sergiser <128863910+EsraSrg@users.noreply.github.com>
2024-10-03 19:04:22 +0300

    commit

--- Language-Fundamentals/16-Metotlar/obj/Debug/net8.0/16-Metotlar.AssemblyInfo.cs
+++ Language-Fundamentals/16-Metotlar/obj/Debug/net8.0/16-Metotlar.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("16-Metotlar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b5d68b63b3801be329829ec92bb73112a926df8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b74db4f11aa67087285ac72428cb97772c0d056")]
 [assembly: System.Reflection.AssemblyProductAttribute("16-Metotlar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("16-Metotlar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
