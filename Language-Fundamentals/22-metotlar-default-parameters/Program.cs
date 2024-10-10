920f45196f372254513070ec63480eafdc0b1536
Esra Sergiser <128863910+EsraSrg@users.noreply.github.com>
2024-10-10 20:04:56 +0300

    default parameters

--- Language-Fundamentals/22-metotlar-default-parameters/Program.cs
+++ Language-Fundamentals/22-metotlar-default-parameters/Program.cs
@@ -1,6 +1,9 @@
 ﻿
 TestMethod();
+Method1();
+Method2(1);
 
+Cevre(3);
 //  parametre olarak almıs oldugu a degiskenin degerini default olarak belirledik.
 //  metot cagirilirken a degiskenine deger verilirse, o deger gelecektir.
 // metot cagırılırken deger verilmezse parametre olarak , default deger ile çalışıcaktır.
@@ -12,4 +15,19 @@ static void TestMethod(int a=10){
     System.Console.WriteLine(a);
 
 
+}
+
+static void Method1( string value="Metot cagırılırken parametre girilmediyse default deger çalışır."){
+
+    System.Console.WriteLine(value);
+}
+
+static void Method2(int a,int b =100){
+    System.Console.WriteLine(a);
+    System.Console.WriteLine(b);
+}
+
+
+static void Cevre(int yariCap, double pi=3.14){
+    System.Console.WriteLine(2*pi*yariCap);
 }
