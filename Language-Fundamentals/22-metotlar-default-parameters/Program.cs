be405ab0d058e29b29b94c40b53abfe18a056728
Esra Sergiser <128863910+EsraSrg@users.noreply.github.com>
2024-10-10 19:45:14 +0300

    default parameters

---
+++ Language-Fundamentals/22-metotlar-default-parameters/Program.cs
@@ -0,0 +1,15 @@
+﻿
+TestMethod();
+
+//  parametre olarak almıs oldugu a degiskenin degerini default olarak belirledik.
+//  metot cagirilirken a degiskenine deger verilirse, o deger gelecektir.
+// metot cagırılırken deger verilmezse parametre olarak , default deger ile çalışıcaktır.
+
+
+
+static void TestMethod(int a=10){
+
+    System.Console.WriteLine(a);
+
+
+}
